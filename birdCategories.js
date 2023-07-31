c64841c350510ce3a11cc34d58d1a2b579f0552d
Taylor Googe <taylorgooge@gmail.com>
2023-07-30 23:24:21 -0400

    Update birdCategories.js

--- birdCategories.js
+++ birdCategories.js
@@ -29,11 +29,6 @@ router.get('/', function (req, res) {
   const query = `SELECT * FROM birdCategories`;
   db.query(query, function (err, result) {
     if (err) throw err;
-    //     res.setHeader("Access-Control-Allow-Origin", "https://birdrfrontend.taylorgooge.repl.co")
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
-    // res.setHeader("Access-Control-Max-Age", "1800");
-    // res.setHeader("Access-Control-Allow-Headers", "content-type");
-    // res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     res.status(200).json(result);
   });
 });
