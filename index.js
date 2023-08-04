756e4fbfc08d607c56700c4426e33aa44af831a8
Taylor Googe <taylorgooge@gmail.com>
2023-08-03 22:46:35 -0400

    Update index.js
    add charts api

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router(); 
 
+const routerApi = require('./restApi');
 const birdCategories = require('./birdCategories')
 const birdCodes = require('./birdCodes')
 const birdSighting = require('./birdSighting')
+const chartApi = require('./chartApi')
 
 
+
+router.use('/rest', routerApi);
 router.use('/birdCategories', birdCategories);
 router.use('/birdCodes', birdCodes);
 router.use('/birdSighting', birdSighting);
+router.use('/data', chartApi);
 
 
 module.exports = router;
