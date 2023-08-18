8003021db7f809aceaaaf41b22b697f29f2ae40d
Taylor Googe <taylorgooge@gmail.com>
2023-08-18 11:46:39 -0400

    Update birdSighting.js

--- birdSighting.js
+++ birdSighting.js
@@ -53,17 +53,6 @@ router.get('/', function(req, res) {
 router.get('/geodist', function(req, res) {
   const { long, lat } = req.body;
   console.log(long, lat)
-//   const query = `SELECT * FROM birdSighting a 
-// WHERE (
-//           acos(sin(a.coordB * 0.0175) * sin(${lat} * 0.0175) 
-//                + cos(a.coordB * 0.0175) * cos(${lat}* 0.0175) *    
-//                  cos((${long} * 0.0175) - (a.coordA * 0.0175))
-//               ) * 6371  <= 10
-//       )`;
-//   db.query(query, function(err, result) {
-//     if (err) throw err;
-//     res.status(200).json(result);
-//   });
 });
 
 
