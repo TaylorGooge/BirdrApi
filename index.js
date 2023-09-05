53c9ead85506b1047ad92f8459fb1725afca5133
Taylor Googe <taylorgooge@gmail.com>
2023-09-05 00:07:54 -0400

    Update index.js
    add checkList support

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const birdCategories = require('./birdCategories')
 const birdCodes = require('./birdCodes')
 const birdSighting = require('./birdSighting')
 const chartApi = require('./chartApi')
+const checklistApi = require('./checklist')
 
 
 router.use('/birdCategories', birdCategories);
 router.use('/birdCodes', birdCodes);
 router.use('/birdSighting', birdSighting);
 router.use('/data', chartApi);
+router.use('/checklists', checklistApi );
 
 
 module.exports = router;
